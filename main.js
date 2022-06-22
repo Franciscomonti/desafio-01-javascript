cf8346c4f9f98210fefaae9764a43f22ba92dfaa
Francsico Monti <montifranciscojavier@gmail.com>
2022-06-21 22:10:51 -0300

    creacion de funciones

--- main.js
+++ main.js
@@ -1,80 +1,86 @@
-let producto1 = 100
-let producto2 = 200
-let producto3 = 300
-let producto4 = 400
+function valorcuota(producto, cantidadcuotas) {
+    return parseInt(producto / cantidadcuotas)
+}
 
-let precio = 0
-let seleccionProducto = 0
+function mostrarMensaje(nombre, precio) {
+    alert("Usted selecciono el producto " + nombre + " valor " + precio)
+}
 
-function valorcuota(producto, cantidadcuotas){
-    return parseInt(producto / cantidadcuotas)}
+function seleccionProducto() {
+    let producto1 = 100
+    let producto2 = 200
+    let producto3 = 300
+    let producto4 = 400
 
-alert("bienvenido a TuTienda !")
+    let seleccionProducto = 0
 
-// seleccion de producto menu de opciones
+    // seleccion de producto menu de opciones
 
-do{
-    seleccionProducto = parseInt(prompt("seleccione el producto deseado :\n1. producto 1\n2. producto 2\n3. producto 3\n4. producto 4"))
-    
-    console.log(seleccionProducto)
+    do {
+        seleccionProducto = parseInt(prompt("seleccione el producto deseado :\n1. producto 1\n2. producto 2\n3. producto 3\n4. producto 4"))
 
-    if(seleccionProducto > 1 , seleccionProducto < 5){
-        switch(seleccionProducto){
+        switch (seleccionProducto) {
             case 1:
-                precio = producto1
-                alert("Usted selecciono el producto 1 valor " + producto1)
-                break
+                mostrarMensaje("1", producto1)
+                return producto1
             case 2:
-                precio = producto2
-                alert("Usted selecciono el producto 2 valor " + producto2)
-                break
+                mostrarMensaje("2", producto2)
+                return producto2
             case 3:
-                precio = producto3
-                alert("Usted selecciono el producto 3 valor " + producto3)
-                break
+                mostrarMensaje("3", producto3)
+                return producto3
             case 4:
-                precio = producto4
-                alert("Usted selecciono el producto 4 valor " + producto4)
+                mostrarMensaje("4", producto4)
+                return producto4
+            default:
+                alert("Usted no selecciono ningun producto")
                 break
         }
-    }
-    else{
-    alert("Usted no selecciono ningun producto")
-    }
 
-}while(seleccionProducto < 1 || seleccionProducto > 4 || isNaN(seleccionProducto))
+    } while (seleccionProducto < 1 || seleccionProducto > 4 || isNaN(seleccionProducto))
+}
 
-// sleccion de cantidad de cuotas
+function seleccionCuotas(precio) {
+    // seleccion de cantidad de cuotas
 
-let seleccionCuotas = 0
+    let seleccionCuotas = 0
 
-do{
-    seleccionCuotas = parseInt(prompt("seleccione cantidad de cuotas:\n1. Un pago sin interes\n2. Tres pagos sin interes\n3. Seis pagos sin interes"))
+    do {
+        seleccionCuotas = parseInt(prompt("seleccione cantidad de cuotas:\n1. Un pago sin interes\n2. Tres pagos sin interes\n3. Seis pagos sin interes"))
 
-    if(seleccionCuotas == 1){
-        alert("Usted selecciono un pago sin interes\n" + "valor de la cuota es : $ " + valorcuota(precio, 1))
-    }
-    else if(seleccionCuotas == 2){
-        alert("Usted selecciono tres pagos sin interes\n" + "valor de la cuota es : $ " + valorcuota(precio, 3))
-    }
-    else if(seleccionCuotas == 3){
-        alert("Usted selecciono seis pagos sin interes\n" + "valor de la cuota es : $ " + valorcuota(precio, 6))
-    }
-    else{
-        alert("Usted no selecciono un valor de cuotas valido")
-    }
+        if (seleccionCuotas == 1) {
+            alert("Usted selecciono un pago sin interes\n" + "valor de la cuota es : $ " + valorcuota(precio, 1))
+        } else if (seleccionCuotas == 2) {
+            alert("Usted selecciono tres pagos sin interes\n" + "valor de la cuota es : $ " + valorcuota(precio, 3))
+        } else if (seleccionCuotas == 3) {
+            alert("Usted selecciono seis pagos sin interes\n" + "valor de la cuota es : $ " + valorcuota(precio, 6))
+        } else {
+            alert("Usted no selecciono un valor de cuotas valido")
+        }
 
-}while(seleccionCuotas < 1 || seleccionCuotas > 3 || isNaN(seleccionCuotas))
+    } while (seleccionCuotas < 1 || seleccionCuotas > 3 || isNaN(seleccionCuotas))
+}
 
-// confirmacion de compra 
+function confirmar() {
+    // confirmacion de compra 
 
-let confirmacion = prompt("Confirma la compra ? Si/No")
+    let confirmacion = prompt("Confirma la compra ? Si/No")
 
-    if(confirmacion == "si"){
+    if (confirmacion.toLowerCase() == "si") {
         alert("Muchas gracias por su compra !")
-    }
-    else{
+    } else {
         alert("Gracias por su visita !")
     }
+}
 
+function principal() {
+    alert("bienvenido a TuTienda !")
 
+    let precio = seleccionProducto()
+
+    seleccionCuotas(precio)
+
+    confirmar()
+}
+
+principal()
